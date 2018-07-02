5d647bc7957854c44e6fe21ac74e99dc77a47834
Ivan Ivic <ivan@basiq.io>
2018-07-02 14:36:30 +0200

    Fixed the issue with connectionId.

--- app.js
+++ app.js
@@ -1,216 +1,196 @@
 /**
- * Object constructor. Requires userId and accessToken params to be provided
- * either by position or by object key.
+ * Object constructor. Requires an object, which should contain keys userId and accessToken.
  *
- * @param userId
- * @param accessToken
- * @param domElement
+ * @param user
  * @constructor
  */
-var Basiq = function (userId, accessToken, domElement, demo, connectionId) {
-    var self = this;
-
-    if (typeof userId === "object") {
-        accessToken = userId.accessToken;
-        domElement = userId.domElement;
-        demo = userId.demo;
-        userId = userId.userId;
-        connectionId = userId.connectionId;
-    }
-
-    if (!userId && !accessToken && demo !== true) {
-        throw new Error("You need to pass the user id and access token to the control");
-    }
-
-    this.host = "http://js.basiq.io/";
-    this.userId = userId;
-    this.accessToken = accessToken;
-    if (connectionId) {
-        this.connectionId = connectionId;
-    }
-    this.url = this.host + "index.html?iframe=true&user_id=" + this.userId + "&access_token=" + this.accessToken;
-    if (demo === true) {
-        this.url += "&demo=true";
-    }
-    if (this.connectionId) {
-        this.url += "&connection_id=" + this.connectionId;
-    }
-    this.domElement = null;
-    this.initialized = false;
-    this.rendered = false;
-
-    this.listeners = {};
-
+var Basiq = function(data) {
+  if (!data.userId && !data.accessToken && data.demo !== true) {
+    throw new Error("You need to pass the user id and access token to the control");
+  }
+  function initializeDomElement(domElementId) {
     if (!domElement) {
-        try {
-            domElement = document.getElementsByTagName("body")[0];
-        } catch (e) {
-            console.error("No body element found in your document", e);
-        }
-    } else if (typeof domElement === "string") {
-        domElement = document.getElementById(domElement);
+      try {
+        return document.getElementsByTagName("body")[0];
+      } catch (e) {
+        throw new Error("No body element found in your document", e);
+      }
     }
+    return document.getElementById(domElementId);
+  }
 
-    self.domElement = domElement;
+  var url = "http://js.basiq.io/index.html?iframe=true&user_id=" + data.userId + "&access_token=" + data.accessToken;
+  if (data.demo === true) {
+    url += "&demo=true";
+  }
+  if (data.connectionId) {
+    url += "&connection_id=" + data.connectionId;
+  }
 
+  var self = this;
+  self.domElement = initializeDomElement(data.domElementId);
+  this.url = url;
+  this.rendered = false;
+  this.listeners = {};
+  this.initialized = false;
 
-    /**
+  /**
      * Initialises postMessage listener
      *
      * @returns {Basiq}
      */
-    var init = function () {
-        var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent",
-            eventer = window[eventMethod],
-            messageEvent = eventMethod === "attachEvent" ? "onmessage" : "message";
+  var init = function() {
+    var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent",
+      eventer = window[eventMethod],
+      messageEvent = eventMethod === "attachEvent" ? "onmessage" : "message";
 
-        eventer(messageEvent, function (e) {
-            var data = JSON.parse(e.data);
+    eventer(messageEvent, function(e) {
+      var data = JSON.parse(e.data);
 
-            for (var event in self.listeners) {
-                if (!self.listeners.hasOwnProperty(event)) {
-                    continue;
-                }
-                var cbs = self.listeners[event];
+      for (var event in self.listeners) {
+        if (!self.listeners.hasOwnProperty(event)) {
+          continue;
+        }
+        var cbs = self.listeners[event];
 
-                if (event === data.event) {
-                    cbs.forEach(function (cb) {
-                        cb(data.payload, event);
-                    });
-                }
-            }
-        });
+        if (event === data.event) {
+          cbs.forEach(function(cb) {
+            cb(data.payload, event);
+          });
+        }
+      }
+    });
 
-        self.initialized = true;
+    self.initialized = true;
+    return self;
+  };
 
-        return self;
-    };
+  init();
 
-    init();
-
-    /**
+  /**
      * Attaches a listener to an event emitted by the frame
      *
      * @param events Array
      * @param cb Function
      * @returns {Basiq}
      */
-    this.addListener = function (events, cb) {
-        if (typeof cb !== "function") {
-            throw new Error("Passed callback must be a function");
-        }
+  this.addListener = function(events, cb) {
+    if (typeof cb !== "function") {
+      throw new Error("Passed callback must be a function");
+    }
 
-        if (!(events instanceof Array)) {
-            events = [events];
-        }
+    if (!(events instanceof Array)) {
+      events = [events];
+    }
 
-        events.forEach(function (event) {
-            self.registerHandler(event, cb);
-        });
+    events.forEach(function(event) {
+      self.registerHandler(event, cb);
+    });
 
-        return self;
-    };
+    return self;
+  };
 
-    this.registerHandler = function (event, cb) {
-        if (!self.listeners[event]) {
-            self.listeners[event] = [];
-        }
+  this.registerHandler = function(event, cb) {
+    if (!self.listeners[event]) {
+      self.listeners[event] = [];
+    }
 
-        self.listeners[event].push(cb);
-    };
+    self.listeners[event].push(cb);
+  };
 
-    /**
+  /**
      * Shows the control
      *
      * @returns {Basiq}
      */
-    this.show = function () {
-        if (!self.rendered) {
-            throw new Error("Component has not been rendered");
-        }
+  this.show = function() {
+    if (!self.rendered) {
+      throw new Error("Component has not been rendered");
+    }
 
-        self.backdrop.style.visibility = "visible";
+    self.backdrop.style.visibility = "visible";
 
-        return self;
-    };
+    return self;
+  };
 
-    /**
+  /**
      * Hides the control
      *
      * @returns {Basiq}
      */
-    this.hide = function () {
-        if (!self.rendered) {
-            throw new Error("Component has not been rendered");
-        }
+  this.hide = function() {
+    if (!self.rendered) {
+      throw new Error("Component has not been rendered");
+    }
 
-        self.backdrop.style.visibility = "hidden";
+    self.backdrop.style.visibility = "hidden";
 
-        return self;
-    };
+    return self;
+  };
 
-    /**
+  /**
      * Destroys the control
      * @returns {Basiq}
      */
-    this.destroy = function () {
-        if (!self.rendered || !self.initialized) {
-            throw new Error("Component has not been rendered");
-        }
+  this.destroy = function() {
+    if (!self.rendered || !self.initialized) {
+      throw new Error("Component has not been rendered");
+    }
 
-        self.domElement.removeChild(self.backdrop);
-        self.backdrop = null;
-        self.container = null;
-        self.initialized = false;
-        self.rendered = false;
-        self.listeners = {};
+    self.domElement.removeChild(self.backdrop);
+    self.backdrop = null;
+    self.container = null;
+    self.initialized = false;
+    self.rendered = false;
+    self.listeners = {};
 
-        return self;
-    };
+    return self;
+  };
 
-    /**
+  /**
      * Renders the control by attaching the backdrop div to the
      * given dom element
      *
      * @returns {Basiq}
      */
-    this.render = function () {
-        if (self.rendered === true) {
-            self.show();
+  this.render = function() {
+    if (self.rendered === true) {
+      self.show();
 
-            return self;
-        }
+      return self;
+    }
 
-        var backdrop = document.createElement("div"),
-            container = document.createElement("div"),
-            iframe = document.createElement("iframe");
+    var backdrop = document.createElement("div"),
+      container = document.createElement("div"),
+      iframe = document.createElement("iframe");
 
-        self.backdrop = backdrop;
-        self.container = container;
-        self.backdrop.style = "position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 100; background: rgba(0, 0, 0, 0.4);";
-        self.container.style = "position: fixed; top: 50%; left: 50%; margin-left: -153px; margin-top: -225px; width: 307px; height: 450px; border-radius: 15px; z-index: 101; -webkit-box-shadow: 0 2px 4px 0 rgba(0,0,0,0.50); -moz-box-shadow: 0 2px 4px 0 rgba(0,0,0,0.50); box-shadow: 0 2px 4px 0 rgba(0,0,0,0.50);";
+    self.backdrop = backdrop;
+    self.container = container;
+    self.backdrop.style =
+      "position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 100; background: rgba(0, 0, 0, 0.4);";
+    self.container.style =
+      "position: fixed; top: 50%; left: 50%; margin-left: -153px; margin-top: -225px; width: 307px; height: 450px; border-radius: 15px; z-index: 101; -webkit-box-shadow: 0 2px 4px 0 rgba(0,0,0,0.50); -moz-box-shadow: 0 2px 4px 0 rgba(0,0,0,0.50); box-shadow: 0 2px 4px 0 rgba(0,0,0,0.50);";
 
+    iframe.src = self.url;
+    iframe.id = "basiq-modal-frame";
+    iframe.frameBorder = "0";
+    iframe.style = "width: 100%; height: 100%; border-radius: 15px;";
 
-        iframe.src = self.url;
-        iframe.id = "basiq-modal-frame";
-        iframe.frameBorder = "0";
-        iframe.style = "width: 100%; height: 100%; border-radius: 15px;";
+    backdrop.id = "basiq-modal-container-backdrop";
+    container.id = "basiq-modal-container";
+    container.appendChild(iframe);
+    backdrop.appendChild(container);
+    self.domElement.appendChild(backdrop);
 
-        backdrop.id = "basiq-modal-container-backdrop";
-        container.id = "basiq-modal-container";
-        container.appendChild(iframe);
-        backdrop.appendChild(container);
-        self.domElement.appendChild(backdrop);
+    self.rendered = true;
+    self.initialized = true;
 
-        self.rendered = true;
-        self.initialized = true;
+    self.addListener(["cancellation", "completion"], function() {
+      self.destroy();
+    });
 
-        self.addListener(["cancellation", "completion"], function () {
-            self.destroy();
-        });
-
-        return self;
-    };
+    return self;
+  };
 };
 
 window.Basiq = Basiq;
