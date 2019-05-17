03524c4821fe04e77c9e51416b391c65e6072b9f
Bogdan Nikolic <bogdan@basiq.io>
2019-05-17 10:20:29 +0200

    Upddating webpack as old webpack contained vulnerabilities

--- webpack.config.js
+++ webpack.config.js
@@ -6,15 +6,13 @@ module.exports = {
         //"bundle": "./app.js",
         "basiq.client.min": "./app.js",
     },
+    mode: "production",
     devtool: "source-map",
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: "[name].js"
     },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            include: /\.min\.js$/,
-            minimize: true
-        })
-    ]
+    optimization: {
+        minimize: true
+      }
 };
